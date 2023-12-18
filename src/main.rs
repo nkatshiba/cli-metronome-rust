42ef7fae0c1e02132ac4bfa08baa3f491a2263c1
nkatshiba <nkatshiba@proton.me>
2023-12-18 22:27:58 +0100

    feat(keybinds): keybinds working

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 use rodio::{Decoder, OutputStream, source::Source};
 use std::{io, thread, time::Duration, fs::File, io::Read};
+use crossterm::{
+    event::{poll, read, Event, KeyCode},
+    terminal::{disable_raw_mode, enable_raw_mode},
+};
 
 fn main() {
+
     // Get BPM from user
-    let bpm = get_bpm();
+    let mut bpm = get_bpm();
+
+    // Enable raw mode to read user input without pressing enter
+    enable_raw_mode().unwrap();
 
     // Calculate the delay time in milliseconds
-    let delay_time = 60_000 / bpm;
+    let mut delay_time = 60_000 / bpm;
 
     // Initialize audio output stream
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
@@ -24,23 +32,51 @@ fn main() {
     // Initialize beat counter
     let mut beat_counter = 1;
 
-    // Infinite loop to simulate the metronome
     loop {
-        // Play the appropriate sound by creating a new Decoder each time
-        let cursor = if beat_counter == 1 {
-            io::Cursor::new(buffer_high.clone())
-        } else {
-            io::Cursor::new(buffer_bright.clone())
-        };
-        let source = Decoder::new(cursor).unwrap().convert_samples();
-        stream_handle.play_raw(source).unwrap();
 
-        // Sleep for the calculated delay time
-        thread::sleep(Duration::from_millis(delay_time as u64));
+      // Play the appropriate sound by creating a new Decoder each time
+      let cursor = if beat_counter == 1 {
+          io::Cursor::new(buffer_high.clone())
+      } else {
+          io::Cursor::new(buffer_bright.clone())
+      };
+      let source = Decoder::new(cursor).unwrap().convert_samples();
+      stream_handle.play_raw(source).unwrap();
 
-        // Update beat counter
-        beat_counter = if beat_counter < 4 { beat_counter + 1 } else { 1 };
+      // Sleep for the calculated delay time
+      thread::sleep(Duration::from_millis(delay_time as u64));
+
+      // Update beat counter
+      beat_counter = if beat_counter < 4 { beat_counter + 1 } else { 1 };
+
+      // Check if there's any user input
+      if poll(Duration::from_millis(1)).unwrap() {
+          if let Event::Key(event) = read().unwrap() {
+              match event.code {
+                  // Increase BPM when '+' or 'up' is pressed
+                  KeyCode::Char('+') | KeyCode::Up => {
+                      bpm += 1;
+                      delay_time = 60_000 / bpm;
+                  }
+                  // Decrease BPM when '-' or 'down' is pressed
+                  KeyCode::Char('-') | KeyCode::Down => {
+                      if bpm > 1 {
+                          bpm -= 1;
+                          delay_time = 60_000 / bpm;
+                      }
+                  }
+                  // Quit when 'q' is pressed
+                  KeyCode::Char('q') => {
+                      disable_raw_mode().unwrap();
+                      return;
+                  }
+                  _ => {}
+              }
+          }
+      }
     }
+
+    // Infinite loop to simulate the metronome
 }
 
 fn get_bpm() -> u32 {
